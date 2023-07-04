0f295a0811606f74d56052f2b7738c6536fc9536
ijapesigan <ijapesigan@gmail.com>
2023-07-04 17:36:11 +0000

    Automated build.

--- tools/openmx_sys.sh
+++ tools/openmx_sys.sh
@@ -13,13 +13,21 @@ function apt_install() {
 }
 
 # OpenMx system dependencies
-apt_install          \
-    build-essential  \
-    cmake            \
-    libopenblas-dev  \
-    liblapack-dev    \
-    libarpack2-dev   \
-    libsuperlu-dev   \
-    libgsl-dev       \
-    libarmadillo-dev \
-    libeigen3-dev
+#apt_install          \
+#    build-essential  \
+#    cmake            \
+#    libopenblas-dev  \
+#    liblapack-dev    \
+#    libarpack2-dev   \
+#    libsuperlu-dev   \
+#    libgsl-dev       \
+#    libarmadillo-dev \
+#    libeigen3-dev
+
+# curl
+apt-get install -y   \
+    libcurl4-openssl-dev
+
+# openssl
+apt-get install -y   \
+    libssl-dev
