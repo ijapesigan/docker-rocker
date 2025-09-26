0554316efced06b47b3114a2fbf693844db26b76
jeksterslab <learn.jeksterslab@gmail.com>
2025-09-26 19:15:59 +0000

    Automated build.

--- scripts/apptainer.sh
+++ scripts/apptainer.sh
@@ -2,4 +2,21 @@
 
 set -e
 
+#!/bin/bash
+
+set -e
+
+function apt_install() {
+    if ! dpkg -s "$@" >/dev/null 2>&1; then
+        if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
+            apt-get update
+        fi
+        apt-get install -y --no-install-recommends "$@"
+    fi
+}
+
+apt_install  \
+    rpm2cpio \
+    cpio
+
 curl -s https://raw.githubusercontent.com/apptainer/apptainer/main/tools/install-unprivileged.sh | bash -s - /usr/local/bin
